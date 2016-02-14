ff7cfd723d7faab48a175a0db1ab487c2ba3538d
yexianghu <yexianghu@gmail.com>
2016-02-14 21:08:22 +0800

    add android project and feature

---
+++ app/src/androidTest/java/gtd/yezi/com/gtd/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gtd.yezi.com.gtd;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
